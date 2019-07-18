db6bf4c9edab623ef4e022374b6e20d5c287f851
Enric Tobella <etobella@creublanca.es>
2019-07-18 16:04:26 +0200

    [IMP] tests

--- tests/test_bonosmash.py
+++ tests/test_bonosmash.py
@@ -4,6 +4,7 @@ from bonobo.nodes import SetFields
 from bonobo.structs.graphs import Graph
 import unittest
 from mock import patch
+import ast
 
 
 def read():
@@ -18,13 +19,22 @@ class Response:
 class TestBonosmash(unittest.TestCase):
     def test_bonosmash(self):
         graph = Graph()
-        graph.add_chain(read, SetFields(fields=('current', 'last')), Smashing(
-            server='test',
-            token='token',
-            widget='widget',
-            fields=['current', 'last']
+        token = 'TOKKKENNNN'
+        widget = 'bomosmash_test_widget'
+        url = 'http://localhost:3030'
+        graph.add_chain(read, SetFields(fields=('current', 'lst')), Smashing(
+            server=url,
+            token=token,
+            widget=widget,
+            fields=['current', ('lst', 'last')]
         ))
         with patch('requests.post') as mk:
             mk.return_value = Response()
             bonobo.run(graph)
             mk.assert_called()
+            args = mk.call_args_list[0]
+        data = ast.literal_eval(args[1].get('data'))
+        self.assertEqual(data.get('current'), 1)
+        self.assertEqual(data.get('last'), 2)
+        self.assertEqual(data.get('auth_token'), token)
+        self.assertEqual(args[0][0], '%s/widgets/%s' % (url, widget))
