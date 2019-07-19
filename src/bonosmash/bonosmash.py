a87c5c91190b9ddacd6751a5b11185fa57d3fd52
Enric Tobella <etobella@creublanca.es>
2019-07-19 11:35:07 +0200

    [IMP] bonosmash: Allow field from fields

--- src/bonosmash/bonosmash.py
+++ src/bonosmash/bonosmash.py
@@ -15,13 +15,19 @@ class Smashing(Configurable, Writer):
 
     server = Option(default='http://localhost:3030')
     token = Option(type=str, required=True)
-    widget = Option(type=str, required=True)
+    widget_field = Option(type=str, default=False)
+    widget = Option(type=str, default=False)
     fields = Option(type=list, required=True)
 
     def send(self, context, *values):
         fields = context.get_input_fields()
         vals = OrderedDict(zip(fields, values))
         data = {'auth_token': self.token}
+        widget = False
+        if self.widget:
+            widget = self.widget
+        elif self.widget_field:
+            widget = vals[self.widget_field]
         for field in self.fields:
             field_name = field
             field_data = field
@@ -29,7 +35,7 @@ class Smashing(Configurable, Writer):
                 field_name = field[0]
                 field_data = field[1]
             data[field_data] = vals[field_name]
-        url = "%s/widgets/%s" % (self.server, self.widget)
+        url = "%s/widgets/%s" % (self.server, widget)
         headers = {'Content-type': 'application/json'}
         requests.post(
             url, data=json.dumps(data), headers=headers
